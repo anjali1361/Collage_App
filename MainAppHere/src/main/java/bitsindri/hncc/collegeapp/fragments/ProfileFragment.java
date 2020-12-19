dd73e5d3a6d01f7917a3fd387eb186029a649a9b
Anjali <anjalikumari13617@gmail.com>
2020-12-19 21:14:46 +0530

    added logout for profile

--- MainAppHere/src/main/java/bitsindri/hncc/collegeapp/fragments/ProfileFragment.java
+++ MainAppHere/src/main/java/bitsindri/hncc/collegeapp/fragments/ProfileFragment.java
@@ -1,21 +1,33 @@
 package bitsindri.hncc.collegeapp.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import bitsindri.hncc.collegeapp.R;
+import bitsindri.hncc.collegeapp.activities.LoginActivity;
 
 
 public class ProfileFragment extends Fragment {
+
+    public TextView log_out;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view=inflater.inflate(R.layout.fragment_profile, container, false);
+
+        log_out.setOnClickListener(v->{
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            startActivity(intent);
+            getActivity().finish();
+        });
+        return  view;
     }
 }
